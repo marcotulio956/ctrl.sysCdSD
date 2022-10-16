92ab052e8ddcac9af4882dbfbbb4720563cb4a7e
Marco Túlio S. M <marcotulio.sm956@gmail.com>
2022-10-16 16:17:59 -0300

    some sys

---
+++ missile_responses.m
@@ -0,0 +1,38 @@
+close all
+clc
+clear
+w = 1.109982
+T = 8.1053
+z = 4.8715
+
+t = 0:0.01:10
+
+s=tf('s');
+
+gs = tf([w^2*T, w^2],[1, 2*z*w, w^2])
+
+us=1
+
+sys=gs*us
+
+step(sys,t)
+
+hold on 
+
+us2=0.7
+
+sys2=gs*us2
+
+step(sys2,t)
+
+hold on 
+
+us3=0.3
+
+sys3=gs*us3
+
+step(sys3,t)
+
+legend("1(t-1)","1(t-2)","1(t-3)")
+
+title("Degraus Diferentes Atrasados")
