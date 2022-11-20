74d79aee8c54d0f2f633ce4bb93d2ee202e5f21e
Marco Túlio S. M <marcotulio.sm956@gmail.com>
2022-11-20 19:06:37 -0300

    time constant

---
+++ labs/estimate.m
@@ -0,0 +1,33 @@
+close all
+clc
+clear
+
+current_folder = pwd;
+vs = readtable ( strcat( current_folder,'/ALL0003/F0003CH2.CSV.xlsx' ) )
+
+vst = table2array(vs (:,4));
+vsv = table2array(vs (:,5));
+
+slice = [295 333]
+
+
+figure;
+plot (vst, vsv);
+hold on;
+title('Grafico vs x tempo');
+xlabel('Tempo');
+ylabel('Tensao');
+
+v0 = vsv(1); vn = vsv(end);  
+
+dv =  v0 - vn;
+
+% zsixtythree = dv * 0.63;
+zthirdysev = dv * 0.37;
+
+% search vsv row corresponding with zsixtythree
+
+tau = vst(333) - vst(295)
+
+
+
